13af6e08c20ee394d8607e2b794489585d61b459
Vital Teresh <vster2006@gmail.com>
2017-06-14 13:28:37 +0300

    modif 172,174,175,186,86

--- p186.m
+++ p186.m
@@ -3,5 +3,5 @@ y0=besselj(0,x);
 y1=besselj(1,x);
 y2=besselj(2,x); 
 y3=besselj(3,x);
-plot(x,y0,'-m',x,y1,'—r',x,y2,'-.k',x,y3,':b')
+plot(x,y0,'-m',x,y1,'-r',x,y2,'-.k',x,y3,'-.b')
 legend('besselj(0,x)','besselj(1,x)','besselj(2,x)', 'besselj(3,x)');
